3dcc27e04527032785ade93a4eec926142e157fc
HarmonyMorse <harmonythedev@gmail.com>
2023-11-17 09:53:06 -0800

    Complete part 4 JS.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 /*----- constants -----*/
-
+const COLORS = {
+    '0': 'white',
+    '1': 'blue',
+    '-1': 'green',
+}
 
 /*----- state variables -----*/
 let board; // Array of 7 col arrays
@@ -7,7 +11,9 @@ let turn; // 1 or -1
 let winner; // null = no winner); 1 or -1 = winner; 'T' = tied game
 
 /*----- cached elements  -----*/
-
+const messageEl = document.querySelector('h1');
+const playAgainBtn = document.querySelector('button');
+const markerEls = document.querySelectorAll('#markers > div');
 
 /*----- event listeners -----*/
 
@@ -33,5 +39,42 @@ function init() {
     render();
 }
 
+// Visualize all staet in the DOM
 function render() {
+    renderBoard();
+    renderMessage();
+    renderControls(); // Hide and show UI elements
+}
+
+function renderBoard() {
+    board.forEach(function(colArr, colIdx) {
+        // Iterate over the cells in the cur column (colArr)
+        colArr.forEach(function(cellVal, rowIdx) {
+            const cellID = `c${colIdx}r${rowIdx}`;
+            const cellEl = document.getElementById(cellID);2
+            cellEl.style.backgroundColor = COLORS[cellVal];
+        });
+    });
+}
+
+function renderMessage() {
+    if (winner === 'T') {
+        messageEl.innerText = "It's a Tie!!!";
+    } else if (winner) {
+        messageEl.innerHTML = `<span style="color: ${COLORS[winner]}">${COLORS[winner].toUpperCase()}</span> wins!`;
+    } else {
+        messageEl.innerHTML = `<span style="color: ${COLORS[turn]}">${COLORS[turn].toUpperCase()}</span>'s Turn`;
+    }
+}
+
+function renderControls() {
+    // Ternary expression is the go to when you want 1 of 2 values returned
+    // <conditional exp> ? <truthy exp> : <falsy exp>;
+    playAgainBtn.style.visibility =  winner ? 'visible': 'hidden';
+    // Iterate over the marker elements to hide/show 
+    // according to the column being full (no 0s) or not
+    markerEls.forEach(function(markerEl, colIdx) {
+        const hideMarker = !board[colIdx].includes(0) || winner;
+        markerEl.style.visibility = hideMarker ? "hidden" : "visible";
+    });
 }
