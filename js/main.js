c4ba61c1d8a1385dde3839760659230a161e8431
HarmonyMorse <harmonythedev@gmail.com>
2023-11-17 12:04:00 -0800

    Complete part 5 JS.

--- js/main.js
+++ js/main.js
@@ -13,10 +13,11 @@ let winner; // null = no winner); 1 or -1 = winner; 'T' = tied game
 /*----- cached elements  -----*/
 const messageEl = document.querySelector('h1');
 const playAgainBtn = document.querySelector('button');
-const markerEls = document.querySelectorAll('#markers > div');
+const markerEls = [...document.querySelectorAll('#markers > div')];
 
 /*----- event listeners -----*/
-
+document.getElementById('markers').addEventListener('click', handleDrop);
+playAgainBtn.addEventListener('click', init);
 
 /*----- functions -----*/
 init();
@@ -38,6 +39,28 @@ function init() {
     winner = null;
     render();
 }
+
+// In response to user interaction, update all impacted state, then call render()
+function handleDrop(e) {
+    const colIdx = markerEls.indexOf(e.target);
+    // Guards...
+    if (colIdx === -1) return;
+    // Shortcut to the column array
+    const colArr = board[colIdx];
+    // Find the index of the first 0 in column
+    const rowIdx = colArr.indexOf(0);
+    // Update the board state with the current player value (turn)
+    board[colIdx][rowIdx] = turn;
+    // Switch player turn
+    turn *= -1;
+    // Check for winner
+    winner = getWinner();
+    render();
+}
+
+function getWinner() {
+}
+
 
 // Visualize all staet in the DOM
 function render() {
